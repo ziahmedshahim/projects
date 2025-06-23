999100535d1b21e6f2d53b3a1f9f9a49f4c9f4da
ziahmedshahim <zia.ahmed@impare.cloud>
2025-06-23 05:11:17 +0000

    pushing code to existing repo

--- csrc/filelist.cu
+++ csrc/filelist.cu
@@ -4,12 +4,12 @@ ifeq ($(LDVERSION), 0)
 endif
 
 ARCHIVE_OBJS=
-ARCHIVE_OBJS += _3592109_archive_1.so
-_3592109_archive_1.so : archive.0/_3592109_archive_1.a
+ARCHIVE_OBJS += _3620865_archive_1.so
+_3620865_archive_1.so : archive.0/_3620865_archive_1.a
 	@$(AR) -s $<
-	@$(PIC_LD) -shared  -Bsymbolic $(LD_NORELAX_FLAG)  -o .//../simv.daidir//_3592109_archive_1.so --whole-archive $< --no-whole-archive
+	@$(PIC_LD) -shared  -Bsymbolic $(LD_NORELAX_FLAG)  -o .//../simv.daidir//_3620865_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
-	@ln -sf .//../simv.daidir//_3592109_archive_1.so $@
+	@ln -sf .//../simv.daidir//_3620865_archive_1.so $@
 
 
 
